875a745f7e92432456b0cab6301b2465ebc2e6e3
khushboo <khushboo.narendra@gmail.com>
2022-12-13 12:36:02 +1100

    Tv Show Updates

---
+++ app/src/main/java/com/example/tmdbclient/presentation/tv/TvShowActivity.kt
@@ -0,0 +1,89 @@
+package com.example.tmdbclient.presentation.tv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tmdbclient.R
+import com.example.tmdbclient.databinding.ActivityTvShowBinding
+import com.example.tmdbclient.presentation.di.Injector
+import javax.inject.Inject
+
+
+class TvShowActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: TvShowViewModelFactory
+    private lateinit var tvShowViewModel: TvShowViewModel
+    private lateinit var binding: ActivityTvShowBinding
+    private lateinit var adapter: TvShowAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_tv_show)
+        (application as Injector).createTvShowSubComponent()
+            .inject(this)
+        tvShowViewModel = ViewModelProvider(this, factory)
+            .get(TvShowViewModel::class.java)
+
+        initRecyclerView()
+
+    }
+
+    private fun initRecyclerView(){
+        binding.tvshowRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = TvShowAdapter()
+        binding.tvshowRecyclerView.adapter = adapter
+        diaplayPopularTvShows()
+    }
+
+    private fun diaplayPopularTvShows(){
+        binding.tvshowProgressBar.visibility = View.VISIBLE
+        val responseLiveData = tvShowViewModel.getTvShows()
+        responseLiveData.observe(this, Observer {
+            if (it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.tvshowProgressBar.visibility = View.GONE
+            }else{
+                binding.tvshowProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.update,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateMovies(){
+        binding.tvshowProgressBar.visibility = View.VISIBLE
+        val response = tvShowViewModel.updateTvShows()
+        response.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.tvshowProgressBar.visibility = View.GONE
+            }else{
+                binding.tvshowProgressBar.visibility = View.GONE
+            }
+        })
+    }
+}
